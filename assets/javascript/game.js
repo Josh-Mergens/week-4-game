2e2551dc230fd8419b9777ab06b027764e52055d
Josh Mergens <bluecatonarock@gmail.com>
2018-02-23 17:12:48 -0800

    psuedocode javascript and script rng for target score

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready (function() {
+
+  // selects a random number to be shown at the start of the game
+  // number should be should be between 20 - 100
+  var random = Math.floor(Math.random() * 100) + 1;
+
+  // append random number to target id
+  $("#target").text(random);
+
+  // setting up random numbers for each jewel
+  // random number has to be between 1 - 12
+
+  // decaring variables for tallies
+
+  // reset the game
+
+  // add the wins to the userTotal
+
+  // add the losses to the userTotal
+
+  // set up click for jewels
+
+  // set win/lose conditions
+});
